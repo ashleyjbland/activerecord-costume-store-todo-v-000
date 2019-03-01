7f204790a22df90b4bf2635d2d0e538590517d9a
Ashley Bland <ashleyjbland@gmail.com>
2019-03-01 23:35:10 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,7 +8,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.timestamp :opening_time
-      t.string :closing_time
+      t.timestamp :closing_time
     end
   end
 end
